e33ac232269d0dc7cdf7eb24192f19e8736550cd
Dmitri Zakharov <dmitri_zakharov@ftn.fedex.com>
2013-06-28 16:10:19 -0400

    Switched to MySQL connection.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,7 +4,7 @@
 # --- !Ups
 
 create table address (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   street                    varchar(255),
   number                    varchar(255),
   postal_code               varchar(255),
@@ -14,16 +14,16 @@ create table address (
 ;
 
 create table product (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   ean                       varchar(255),
   name                      varchar(255),
   description               varchar(255),
-  picture                   blob,
+  picture                   longblob,
   constraint pk_product primary key (id))
 ;
 
 create table stock_item (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   warehouse_id              bigint,
   product_id                bigint,
   quantity                  bigint,
@@ -31,13 +31,13 @@ create table stock_item (
 ;
 
 create table tag (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   name                      varchar(255),
   constraint pk_tag primary key (id))
 ;
 
 create table warehouse (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   name                      varchar(255),
   address_id                bigint,
   constraint pk_warehouse primary key (id))
@@ -49,16 +49,6 @@ create table product_tag (
   tag_id                         bigint not null,
   constraint pk_product_tag primary key (product_id, tag_id))
 ;
-create sequence address_seq;
-
-create sequence product_seq;
-
-create sequence stock_item_seq;
-
-create sequence tag_seq;
-
-create sequence warehouse_seq;
-
 alter table stock_item add constraint fk_stock_item_warehouse_1 foreign key (warehouse_id) references warehouse (id) on delete restrict on update restrict;
 create index ix_stock_item_warehouse_1 on stock_item (warehouse_id);
 alter table stock_item add constraint fk_stock_item_product_2 foreign key (product_id) references product (id) on delete restrict on update restrict;
@@ -74,29 +64,19 @@ alter table product_tag add constraint fk_product_tag_tag_02 foreign key (tag_id
 
 # --- !Downs
 
-SET REFERENTIAL_INTEGRITY FALSE;
+SET FOREIGN_KEY_CHECKS=0;
 
-drop table if exists address;
+drop table address;
 
-drop table if exists product;
+drop table product;
 
-drop table if exists product_tag;
+drop table product_tag;
 
-drop table if exists stock_item;
+drop table stock_item;
 
-drop table if exists tag;
+drop table tag;
 
-drop table if exists warehouse;
+drop table warehouse;
 
-SET REFERENTIAL_INTEGRITY TRUE;
-
-drop sequence if exists address_seq;
-
-drop sequence if exists product_seq;
-
-drop sequence if exists stock_item_seq;
-
-drop sequence if exists tag_seq;
-
-drop sequence if exists warehouse_seq;
+SET FOREIGN_KEY_CHECKS=1;
 
